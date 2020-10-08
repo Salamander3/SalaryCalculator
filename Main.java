1011a6f7f82e631f937502720d7cf9168835674a
Salamander3 <benjamin.sentirmay@patelhs.org>
2020-10-08 14:11:45 +0000

    Added name collection code

--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 class Cal {
   public static void main(String[] args) {
-    System.out.println("WORKS");
+    //Adds the Scanner Object
+    Scanner scan = new Scanner(System.in);
+
+    //Collects the user's name
+    System.out.println("Please enter your name.");
+    String name = scan.nextLine();
+    System.out.println("Your name is " + name);
   }
 }
